b631341297145654c384cef0878699af42101da3
Kendus <kendustisdalejeffries@gmail.com>
2018-07-27 12:44:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kendus/quiz_app/Quiz_Questions.java
@@ -0,0 +1,13 @@
+package com.example.kendus.quiz_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quiz_Questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz__questions);
+    }
+}
